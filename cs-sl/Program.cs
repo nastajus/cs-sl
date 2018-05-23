3ef47eba7201f5e1f8338fbd259f0525236d6a05
Ian Nastajus <ian.nastajus@gmail.com>
2018-05-23 14:25:30 -0400

    client & server projects created

--- cs-sl/Program.cs
+++ cs-sl/Program.cs
@@ -69,6 +69,7 @@ namespace cs_sl
             request.Resource = "rate_limit";
 
             IRestResponse response = client.Execute(request);
+            client.Ex
 
             // step 2 : to actually deserialize, must convert to a known type.  Wrote my own "known type" : RateLimitResponseContent class, per the sample json response commented below.
 
@@ -79,11 +80,11 @@ namespace cs_sl
             RateLimitResponseContent rateLimit2 =
                 new RateLimitResponseContent
                 {
-                    rate = new RateLimitResponseContent.Thing {limit = 0, remaining = 0, reset = 0},
+                    rate = new RateLimitResponseContent.Thing { limit = 0, remaining = 0, reset = 0 },
                     resources = new RateLimitResponseContent.Resources
                     {
-                        core = new RateLimitResponseContent.Thing {limit = 0, remaining = 0, reset = 0},
-                        search = new RateLimitResponseContent.Thing {limit = 0, remaining = 0, reset = 0}
+                        core = new RateLimitResponseContent.Thing { limit = 0, remaining = 0, reset = 0 },
+                        search = new RateLimitResponseContent.Thing { limit = 0, remaining = 0, reset = 0 }
                     }
                 };
 
@@ -93,7 +94,7 @@ namespace cs_sl
 
             var rateLimit3 = new
             {
-                rate = new {limit = 0, remaining = 0, reset = 0}, 
+                rate = new { limit = 0, remaining = 0, reset = 0 },
                 //resources = new
                 //{
                 //    core = new {limit = 0, remaining = 0, reset = 0},
@@ -104,115 +105,6 @@ namespace cs_sl
             // nothing happens, type remains with all defaults unchanged
             JsonConvert.PopulateObject(response.Content, rateLimit3);
             JsonConvert.DeserializeAnonymousType(response.Content, rateLimit3);
-
-            //invalid, ref keyword must match as both parameter and argument.
-            //JsonConvert.DeserializeAnonymousType(response.Content, ref rateLimit3);
-
-            int foo = 3;
-            var x = 5;
-            var str =
-@"name:dante
-age:26
-website:http";
-
-            Dog dog = new Doberman();
-            dog.name = "fido";
-
-
-            Type xxx = 1.GetType();
-            Console.WriteLine(xxx);
-            //"System.Int32"
-
-            Type type1 = dog.GetType();
-            Console.WriteLine(type1);
-
-            Type type2 = typeof(Dog);
-            Console.WriteLine(type2);
-            //"sl_cs.Program.Dog"
-
-            Type type3 = typeof(Dog).GetType();
-            Console.WriteLine(type3);
-            //"sl_cs.Program.Dog"
-
-            Type type4 = type1.GetType();
-            Console.WriteLine(type4);
-            //"sl_cs.Program.Dog"
-
-            var typeType = typeof(Type);
-
-            var test1 = type1 == type2;
-            var test2 = type2 == type3;
-            var test3 = type3 == type1;
-
-
-            var dante = new Person();
-
-            
-            var lassie = new Dog();
-            SetNameOfThing(dante, "Dnate");
-            SetNameOfThing(lassie, "Lassie");
-
-
-            DmlPopulate(str,dante);
-        }
-
-        static void SetNameOfThing(object p, string name)
-        {
-            Type pType = p.GetType();
-            if (pType.Name == typeof(Dog).Name || pType.Name == typeof(Person).Name)
-            {
-                //remember the ... doesn't have any info on the instance itself, despite ... coming from instance
-                FieldInfo fi = pType.GetField("name");
-                fi.SetValue(pType, name);
-            }  //System.ArgumentException: 'Field 'name' defined on type 'cs_sl.Program+Person' is not a field on the target object which is of type 'System.RuntimeType'.'
-
-
-        }
-
-        public class Person
-        {
-            public string name;
-            public string age;
-            public string website;
-
-        }
-        public class Dog
-        {
-            public string name;
-            public string age;
-        }
-
-        public class Doberman : Dog
-        {
-            public void Bark()
-            {
-                Console.WriteLine("Woof!");
-            }
-        }
-
-        static void DmlPopulate(string str, object obj) 
-        {
-            var lines = str.Split('\n');
-
-            var fields = new Dictionary<string, FieldInfo>();
-
-            foreach (var fieldInfo in obj.GetType().GetFields())
-            {
-                fields.Add(fieldInfo.Name, fieldInfo);
-            }
-
-            foreach (var line in lines)
-            {
-                var pieces = line.Split(':');
-                if(pieces.Length != 2) throw new ArgumentException("The parameter is not valid DML", nameof(str));
-                var fieldName = pieces[0];
-                if (!fields.ContainsKey(fieldName)) continue;
-                var info = fields[fieldName];
-                info.SetValue(obj, pieces[1]);
-
-                 
-                //"sl_cs.Program.Person.website; Type:string, ByteAlignment:2; Flags:Instace|public"
-            }
         }
     }
 
